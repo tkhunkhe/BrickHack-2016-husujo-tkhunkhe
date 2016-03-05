6af5954a01ec9b8c73a57638035893123d48ffe7
tkhunkhe <tkhunkhe@gmail.com>
2016-03-05 16:44:05 -0500

    delete accelerometor

--- acceleromonitor/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-
-VERSION=$(cat appinfo.json | grep "versionLabel" | cut -d ":" -f 2 | sed 's/[ ",]//g')
-
-REMOTEPATH=$(cat .remotepath)
-
-scp build/acceleromonitor.pbw $REMOTEPATH/acceleromonitor-$VERSION.pbw
